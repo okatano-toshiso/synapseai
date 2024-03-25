65330088a2f133e056c564fef6d454474e264678
okatano-toshiso <web@nuevo.jp>
2024-03-25 15:48:56 +0900

    [modi] bot_css

--- projects/sns_texts/forms.py
+++ projects/sns_texts/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class ChatForm(forms.Form):
 
-    sentence = forms.CharField(label='チャット', widget=forms.Textarea(), required=True)
+    sentence = forms.CharField(label='', widget=forms.Textarea(), required=True)
