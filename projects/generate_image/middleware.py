c89b4245711d082689619813b30a913135bd946a
okatano-toshiso <web@nuevo.jp>
2024-03-27 18:44:26 +0900

    [modi] basic_auth

--- projects/generate_image/middleware.py
+++ projects/generate_image/middleware.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
 from base64 import b64decode
 
 class BasicAuthMiddleware:
@@ -7,22 +6,22 @@ class BasicAuthMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # 特定のアプリケーションまたはビューへのリクエストかどうかをチェック
+        # 特定のパスに対するリクエストに対してのみBasic認証を適用
         if request.path.startswith('/generate_image/'):
-            # BASIC認証の処理
+            # HTTP_AUTHORIZATIONヘッダーが存在するかチェック
             if 'HTTP_AUTHORIZATION' in request.META:
                 auth = request.META['HTTP_AUTHORIZATION'].split()
+                # Basic認証の形式が正しいかチェック
                 if len(auth) == 2 and auth[0].lower() == "basic":
-                    username, password = b64decode(auth[1]).decode('utf-8').split(':')
-                    user = authenticate(request, username=username, password=password)
-                    if user is not None and user.is_active:
-                        login(request, user)
+                    username, password = b64decode(auth[1]).decode('utf-8').split(':', 1)
+                    # 指定されたユーザー名とパスワードで認証
+                    if username == 'fugafuga' and password == 'hogehoge1234':
                         return self.get_response(request)
 
-            # 認証が必要な場合、または認証に失敗した場合
+            # 認証に必要な情報がない、または認証が失敗した場合
             response = HttpResponse("Authorization required", status=401)
             response['WWW-Authenticate'] = 'Basic realm="My Realm"'
             return response
 
-        # その他のリクエストは通常どおり処理
+        # その他のリクエストは通常通り処理
         return self.get_response(request)
