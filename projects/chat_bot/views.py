0c04647a6a2f3f2112a92ee522bc7d0aa1832158
okatano-toshiso <web@nuevo.jp>
2024-04-01 20:53:37 +0900

    [modi] seiri

--- projects/chat_bot/views.py
+++ projects/chat_bot/views.py
@@ -1,33 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render
-# Create your views here.
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
 import os
 from openai import OpenAI
 
 @csrf_exempt
 def ask(request):
-
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    if os.path.exists(dotenv_path):
-        load_dotenv(dotenv_path)
-
-    # Access the API key from the environment
     OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
-
     user_message = request.POST.get('message') 
-    conversation = request.session.get('conversation', [])     # これまでの会話をセッションから取得
-    conversation.append(user_message)     # ユーザーのメッセージを会話に追加
-    prompt = " ".join(conversation)     # コンテキストとしてプロンプトを作成
+    conversation = request.session.get('conversation', [])
+    conversation.append(user_message)
+    prompt = " ".join(conversation)
     client = OpenAI(
-        # This is the default and can be omitted
         api_key = OPENAI_API_KEY,
     )
-
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
