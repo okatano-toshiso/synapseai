25113be35ea0c6a176c81459bf5ff6b416c28825
okatano-toshiso <web@nuevo.jp>
2024-04-29 15:04:16 +0900

    [add] error handling in recipe

--- projects/recipe/views.py
+++ projects/recipe/views.py
@@ -14,8 +14,14 @@ import deepl
 
 
 def index(request):
-    OPENAI_API_KEY = os.environ['OPENAI_API_IMAGE_KEY']
-    DEEPL_API_KEY = os.environ['DEEPL_API_KEY']
+    try:
+        OPENAI_API_KEY = os.environ['OPENAI_API_IMAGE_KEY']
+    except KeyError:
+        return HttpResponse("OPENAI_API_APIキーが設定されていません。", status=500)
+    try:
+        DEEPL_API_KEY = os.environ['DEEPL_API_KEY']
+    except KeyError:
+        return HttpResponse("DEEPL_API_APIキーが設定されていません。", status=500)
     domain = request.build_absolute_uri('/')
     file_path = 'recipe.jpg'
     if default_storage.exists(file_path):
@@ -29,74 +35,77 @@ def index(request):
             image_file = request.FILES['image']
             image_content = image_file.read()
             default_storage.save('recipe.jpg', ContentFile(image_content))
-            client = OpenAI(
-                api_key = OPENAI_API_KEY,
-            )
-            image_path = settings.BASE_DIR / "uploads/recipe.jpg"
-            print(image_path)
-            def encode_image(image_path):
-                with open(image_path, "rb") as image_file:
-                    return base64.b64encode(image_file.read()).decode('utf-8')
-            base64_image = encode_image(image_path)
-            headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {OPENAI_API_KEY}"
-            }
-            payload = {
-            "model": "gpt-4-vision-preview",
-            "messages": [
-                {
-                "role": "user",
-                "content": [
-                    {
-                    "type": "text",
-                    "text": """
-                        What’s in this image?
-                        Just tell me the name of the ingredients in the image.
-                        Please do not answer except for the name of the ingredients.
-                        If there are no ingredients in the picture, please return the message No ingredients.
-                        """
-                    },
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}"
-                    }
-                    }
-                ]
+            try:
+                client = OpenAI(
+                    api_key = OPENAI_API_KEY,
+                )
+                image_path = settings.BASE_DIR / "uploads/recipe.jpg"
+                print(image_path)
+                def encode_image(image_path):
+                    with open(image_path, "rb") as image_file:
+                        return base64.b64encode(image_file.read()).decode('utf-8')
+                base64_image = encode_image(image_path)
+                headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {OPENAI_API_KEY}"
                 }
-            ],
-            "max_tokens": 300
-            }
-            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-            img_results = (response.json().get("choices")[0]["message"]["content"])
-            response = client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[
+                payload = {
+                "model": "gpt-4-vision-preview",
+                "messages": [
                     {
-                        "role": "system",
-                        "content": """
-                            You are a cookery researcher.
-                            USER gives you an ingredient.
-                            Please give us a recipe that can be cooked from that ingredient.
-                        """
-                    },
-                    {
-                        "role": "user",
-                        "content": "Cooking ingredients" + img_results
-                    },
+                    "role": "user",
+                    "content": [
+                        {
+                        "type": "text",
+                        "text": """
+                            What’s in this image?
+                            Just tell me the name of the ingredients in the image.
+                            Please do not answer except for the name of the ingredients.
+                            If there are no ingredients in the picture, please return the message No ingredients.
+                            """
+                        },
+                        {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                        }
+                    ]
+                    }
                 ],
-            )
-            recipe_results = response.choices[0].message.content
-
-            def translate_text_with_deepl(text, auth_key):
-                translator = deepl.Translator(auth_key)
-                result = translator.translate_text(text, target_lang="JA")
-                return result.text
-            recipe_results = translate_text_with_deepl(recipe_results, DEEPL_API_KEY)
-            recipe_results = recipe_results.replace("\n", "<br />")
-            img_results = translate_text_with_deepl(img_results, DEEPL_API_KEY)
-
+                "max_tokens": 300
+                }
+                response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+                img_results = (response.json().get("choices")[0]["message"]["content"])
+                response = client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": """
+                                You are a cookery researcher.
+                                USER gives you an ingredient.
+                                Please give us a recipe that can be cooked from that ingredient.
+                            """
+                        },
+                        {
+                            "role": "user",
+                            "content": "Cooking ingredients" + img_results
+                        },
+                    ],
+                )
+                recipe_results = response.choices[0].message.content
+                def translate_text_with_deepl(text, auth_key):
+                    translator = deepl.Translator(auth_key)
+                    result = translator.translate_text(text, target_lang="JA")
+                    return result.text
+                recipe_results = translate_text_with_deepl(recipe_results, DEEPL_API_KEY)
+                recipe_results = recipe_results.replace("\n", "<br />")
+                img_results = translate_text_with_deepl(img_results, DEEPL_API_KEY)
+            except Exception as e:
+                return HttpResponse(f"API呼び出し中にエラーが発生しました: {str(e)}", status=500)
+        else:
+            return HttpResponse("フォームのデータが無効です。", status=400)
     else:
         form = ChatForm()
     template = loader.get_template('recipe/index.html')
