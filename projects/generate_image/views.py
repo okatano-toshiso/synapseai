8782e5e2030cd0df435b0016c968ad4128b89e25
okatano-toshiso <web@nuevo.jp>
2024-04-29 14:43:52 +0900

    [add] error handling in generate_image

--- projects/generate_image/views.py
+++ projects/generate_image/views.py
@@ -10,32 +10,46 @@ import json
 
 @login_required
 def index(request):
-    OPENAI_API_KEY = os.environ['OPENAI_API_IMAGE_KEY']
-    DEEPL_API_KEY = os.environ['DEEPL_API_KEY']
+    try:
+        OPENAI_API_KEY = os.environ['OPENAI_API_IMAGE_KEY']
+    except KeyError:
+        return HttpResponse("OPENAIのAPIキーが設定されていません。", status=500)
+    try:
+        DEEPL_API_KEY = os.environ['DEEPL_API_KEY']
+    except KeyError:
+        return HttpResponse("DEEPLのAPIキーが設定されていません。", status=500)
     image_url = ""
     if request.method == "POST":
         form = ChatForm(request.POST)
         if form.is_valid():
-            initial_message = 'I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:'
-            sentence = form.cleaned_data['sentence']
-            def translate_text_with_deepl(text, auth_key):
-                translator = deepl.Translator(auth_key)
-                result = translator.translate_text(text, target_lang="EN-US")
-                return result.text
-            sentence = translate_text_with_deepl(sentence, DEEPL_API_KEY)
-            sentence = initial_message + sentence
-            print(sentence)
-            client = OpenAI(
-                api_key = OPENAI_API_KEY,
-            )
-            response =  client.images.generate(
-                model   = "dall-e-3",
-                prompt  = sentence,
-                n       = 1,
-                size="1024x1024",
-            )
-            image_url = response.data[0].url
-            print(response.data)
+            try:
+                initial_message = 'I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:'
+                sentence = form.cleaned_data['sentence']
+                def translate_text_with_deepl(text, auth_key):
+                    translator = deepl.Translator(auth_key)
+                    result = translator.translate_text(text, target_lang="EN-US")
+                    return result.text
+                sentence = translate_text_with_deepl(sentence, DEEPL_API_KEY)
+                sentence = initial_message + sentence
+                print(sentence)
+            except Exception as e:
+                return HttpResponse(f"DEEPL_APIのAPI呼び出し中にエラーが発生しました: {str(e)}", status=500)
+            try:
+                client = OpenAI(
+                    api_key = OPENAI_API_KEY,
+                )
+                response =  client.images.generate(
+                    model   = "dall-e-3",
+                    prompt  = sentence,
+                    n       = 1,
+                    size="1024x1024",
+                )
+                image_url = response.data[0].url
+                print(response.data)
+            except Exception as e:
+                return HttpResponse(f"OPENAI_APIのAPI呼び出し中にエラーが発生しました: {str(e)}", status=500)
+        else:
+            return HttpResponse("フォームのデータが無効です。", status=400)
     else:
         form = ChatForm()
     domain = request.build_absolute_uri('/')
