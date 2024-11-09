a4cdbbbd21a090c811d33e22322a1d33945b5298
okatano-toshiso <web@nuevo.jp>
2024-11-09 11:02:02 +0900

    [modi] music_review

--- projects/music_review/forms.py
+++ projects/music_review/forms.py
@@ -1,29 +1,36 @@
 from django import forms
 
 class ChatForm(forms.Form):
-    title = forms.CharField(label='アルバム名', widget=forms.TextInput(), required=False)
-    artist = forms.CharField(label='アーティスト', widget=forms.TextInput(), required=False)
-    producer = forms.CharField(label='プロデューサー', widget=forms.TextInput(), required=False)
-    genre = forms.CharField(label='ジャンル', widget=forms.TextInput(), required=False)
-    label = forms.CharField(label='レーベル', widget=forms.TextInput(), required=False)
+    title = forms.CharField(label='TITLE', widget=forms.TextInput(), required=False)
+    artist = forms.CharField(label='ARTIST', widget=forms.TextInput(), required=False)
+    producer = forms.CharField(label='PRODUCER', widget=forms.TextInput(), required=False)
+    genre = forms.CharField(label='GENRE', widget=forms.TextInput(), required=False)
+    label = forms.CharField(label='LABEL', widget=forms.TextInput(), required=False)
     release = forms.DateField(
-        label='リリース日',
+        label='RELEASE',
         widget=forms.DateInput(attrs={'type': 'date'}),  # ブラウザの日付選択ウィジェットを使用
         required=False
     )
-    review = forms.CharField(
-        label='評論',
-        widget=forms.Textarea(attrs={'placeholder': 'レビュー'}),
+    spotyfi = forms.CharField(label='SPOTYFI', widget=forms.TextInput(), required=False)
+    youtube = forms.CharField(label='YOUTUBE', widget=forms.TextInput(), required=False)
+    biography = forms.CharField(
+        label='BIOGRAPHY',
+        widget=forms.Textarea(attrs={'placeholder': 'BIOGRAPHY'}),
         required=True
     )
-    recommend = forms.CharField(label='おすすめ', widget=forms.TextInput(), required=False)
+    review = forms.CharField(
+        label='REVIEW',
+        widget=forms.Textarea(attrs={'placeholder': 'REVIEW'}),
+        required=True
+    )
+    recommend = forms.CharField(label='RECOMMEND', widget=forms.TextInput(), required=False)
     track_list = forms.CharField(
-        label='収録曲',
-        widget=forms.Textarea(attrs={'placeholder': '収録曲'}),
+        label='TRACKLISTS',
+        widget=forms.Textarea(attrs={'placeholder': 'TRACKLISTS'}),
         required=True
     )
     members = forms.CharField(
-        label='参加者',
-        widget=forms.Textarea(attrs={'placeholder': '参加メンバー'}),
+        label='PERSONAL',
+        widget=forms.Textarea(attrs={'placeholder': 'PERSONAL'}),
         required=False
     )
--- projects/music_review/views.py
+++ projects/music_review/views.py
@@ -3,6 +3,20 @@ from django.template import loader
 from .forms import ChatForm
 import os
 from openai import OpenAI
+import re
+from bs4 import BeautifulSoup
+import requests
+
+def clean_and_format_members(member_list):
+    members = member_list.split("\n")
+    cleaned_members = []
+    for member in members:
+        cleaned_member = re.sub(r'\s*–.*$', '', member)
+        cleaned_member = cleaned_member.replace(" ", "")
+        cleaned_member = re.sub(r'：.*$', '', cleaned_member)
+        cleaned_member = f"#{cleaned_member}"
+        cleaned_members.append(cleaned_member)
+    return "\n".join(cleaned_members)
 
 def index(request):
     try:
@@ -13,11 +27,17 @@ def index(request):
     if request.method == "POST":
         form = ChatForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data['title']
-            artist = form.cleaned_data['artist']
-            genre = form.cleaned_data['genre']
-            producer  = form.cleaned_data['producer']
-            label = form.cleaned_data['label']
+            title = form.cleaned_data['title'].replace(" ", "")
+            if not form.cleaned_data['artist'].replace(" ", "").startswith('#'):
+                artist = "#" + re.sub(r',\s*', ' #', form.cleaned_data['artist'].replace(" ", ""))
+            if not form.cleaned_data['genre'].replace(" ", "").startswith('#'):
+                genre = "#" + re.sub(r',\s*', ' #', form.cleaned_data['genre'].replace(" ", ""))
+            if not form.cleaned_data['producer'].replace(" ", "").startswith('#'):
+                producer = "#" + re.sub(r',\s*', ' #', form.cleaned_data['producer'].replace(" ", ""))
+            spotyfi  = form.cleaned_data['spotyfi']
+            youtube  = form.cleaned_data['youtube']
+            if not form.cleaned_data['label'].replace(" ", "").startswith('#'):
+                label = "#" + re.sub(r',\s*', ' #', form.cleaned_data['label'].replace(" ", ""))
             recommend = form.cleaned_data['recommend']
             track_list = form.cleaned_data['track_list']
             members = form.cleaned_data['members']
@@ -27,36 +47,126 @@ def index(request):
             else:
                 release = ''
             review = form.cleaned_data['review']
+            biography = form.cleaned_data['biography']
+
+
+
+            def biography_scrape_article(url):
+                joined_text = ""
+                response = requests.get(url)
+                response.encoding = response.apparent_encoding
+                soup = BeautifulSoup(response.text, "html.parser")
+                content_div = soup.find("div", id="mw-content-text", class_="mw-body-content")
+                if content_div:
+                    joined_text += content_div.get_text(separator="\n")  # テキストを改行で区切って取得
+                return joined_text
+            url = "https://en.wikipedia.org/wiki/The_Beach_Boys"
+            biography = biography_scrape_article(url)
+            print(biography)
+
+
+
+
             try:
                 client = OpenAI(
                     api_key = OPENAI_API_KEY,
                 )
                 review = client.chat.completions.create(
-                    model="gpt-3.5-turbo",
+                    model="gpt-4o",
                     messages=[
                         {
                             "role": "system",
                             "content": f"""
-                            導入部
+                            システムは優秀な音楽評論家です。下記の情報をもとにレビューを書いてください。
+                            必ず日本語で回答してください。
+                            必要であれば、{artist}の作品「{title}」についてネットで調べてください。
+                            {artist}のバイオグラフィーは下記になります。
+                            \nーーーーーーーーーーーーーーーーーーーーーー\n
+                            {biography}
+                            \nーーーーーーーーーーーーーーーーーーーーーー\n
+                            {title}の情報は下記になります。
                             \nーーーーーーーーーーーーーーーーーーーーーー\n
                             {review}
                             \nーーーーーーーーーーーーーーーーーーーーーー\n
-                            この音楽作品の評論を書いています。300文字以内に綺麗に校正してまとめてください。
+                            アルバムの説明、アルバムの制作背景、収録曲、世評、社会に与えた影響などがあればレビューしてください。
+                            この音楽作品の評論を書いています。600文字以内に綺麗に校正してまとめてください。
                             """
                         }
                     ],
                 )
                 review_result = review.choices[0].message.content
                 review_result = review_result.replace("\n", "<br>")
-                print(review_result)
 
-                tracks = track_list.split("\n")
-                numbered_track_list = ""
-                for i, track in enumerate(tracks, start=1):
-                    numbered_track_list += f"{i}. {track}<br>"
-                members = members.replace("\n", "<br>")
-                chat_results = "アルバム名:" + title + "<br>アーティスト:" + artist + "<br>ジャンル:" + genre + "<br>プロデューサー:" + producer + "<br>レーベル:" + label + "<br>リリース:" + release + "<br><br>レビュー：<br>" + review_result + "<br><br>おすすめ曲：<br>" + recommend + "<br><br>収録曲：<br>" + numbered_track_list + "<br><br>参加メンバー：<br>" + members
+                # tracklist = track_list.split("\n")
+                # cleaned_tracks = []
+                # for track in tracklist:
+                #     cleaned_track = re.sub(r'^\d+\.\s*', '', track)
+                #     if '"' in cleaned_track:
+                #         cleaned_track = re.sub(r'"[^"]*"$', '', cleaned_track)
+                #         match = re.search(r'"([^"]*)"', cleaned_track)
+                #         if match:
+                #             cleaned_track = match.group(1)
+                #     cleaned_tracks.append(cleaned_track)
+                # renumbered_tracks = []
+                # for i, track in enumerate(cleaned_tracks, start=1):
+                #     renumbered_tracks.append(f"{i}. {track}")
 
+                tracklist = track_list.split("\n")
+                cleaned_tracks = []
+                renumbered_tracks = []
+                track_number = 1
+
+                for track in tracklist:
+                    # DISCの検出（そのまま追加し、次のトラックから再ナンバリング）
+                    if re.match(r'Disc\s*\d+', track):
+                        renumbered_tracks.append("\n")
+                        renumbered_tracks.append(track)
+                        track_number = 1  # DISCの次は1から採番
+                        continue
+                    # トラック番号の除去
+                    cleaned_track = re.sub(r'^\d+\.\s*', '', track)
+                    # 引用符または「」で囲まれたテキストの処理
+                    if '"' in cleaned_track or '「' in cleaned_track:
+                        # 引用符で囲まれたテキストを削除
+                        cleaned_track = re.sub(r'"[^"]*"$', '', cleaned_track)
+                        # cleaned_track = re.sub(r'「[^」]*」.*$', '', cleaned_track)  # 「」内とその後の文字を削除
+                        # 「」または""内のテキストを抽出して代入
+                        match = re.search(r'"([^"]*)"', cleaned_track) or re.search(r'「([^」]*)」', cleaned_track)
+                        if match:
+                            cleaned_track = match.group(1)
+                    # 再ナンバリングしてリストに追加
+                    renumbered_tracks.append(f"{track_number}. {cleaned_track}")
+                    track_number += 1
+
+                formatted_member_list = clean_and_format_members(members)
+                formatted_member_list = formatted_member_list.replace("\n", "<br>")
+
+                chat_results = ""
+                if spotyfi:
+                    chat_results += spotyfi
+                if title:
+                    chat_results += "<br><br>TITLE: #" + title
+                if artist:
+                    chat_results += "<br>ARTIST: " + artist
+                if genre:
+                    chat_results += "<br>GENRE: " + genre
+                if producer:
+                    chat_results += "<br>PRODUCER: " + producer
+                if label:
+                    chat_results += "<br>LABEL: " + label
+                if release:
+                    chat_results += "<br>RELEASE: " + release
+                if youtube:
+                    chat_results += "<br>YOUTUBE: " + youtube
+                if review_result:
+                    chat_results += "<br><br>REVIEW<br>" + review_result
+                if recommend:
+                    chat_results += "<br><br>RECOMMEND<br>" + recommend
+                if renumbered_tracks:
+                    # chat_results += "<br><br>TRACKLISTS<br>" + renumbered_tracks
+                    chat_results += "<br><br>TRACKLISTS<br>" + "<br>".join(renumbered_tracks)
+                if members:
+                    chat_results += "<br><br>PERSONAL<br>" + formatted_member_list
             except Exception as e:
                 return HttpResponse(f"API呼び出し中にエラーが発生しました: {str(e)}", status=500)
         else:
@@ -69,7 +179,10 @@ def index(request):
     context = {
         'form': form,
         'domain': domain,
-        'app_name': 'send_mail',
+        'app_name': 'music_review',
         'chat_results': chat_results
     }
     return HttpResponse(template.render(context, request))
+
+
+
