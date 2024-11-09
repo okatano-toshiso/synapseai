dee257f4f0db13636e6babc40fb5798dbee932eb
okatano-toshiso <web@nuevo.jp>
2024-11-09 18:49:02 +0900

    [modi] style

--- projects/music_review/forms.py
+++ projects/music_review/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class ChatForm(forms.Form):
-    official_site_discography = forms.CharField(label='OFFICIAL_SITE_DISCOGRAPHY', widget=forms.TextInput(), required=False)
-    official_site_biography = forms.CharField(label='OFFICIAL_SITE_BIOGRAPHY', widget=forms.TextInput(), required=False)
-    wiki_discography = forms.CharField(label='WIKI_DISCOGRAPHY', widget=forms.TextInput(), required=False)
-    wiki_biography = forms.CharField(label='WIKI_BIOGRAPHY', widget=forms.TextInput(), required=False)
+    official_site_discography = forms.CharField(label='DISCOGRAPHY', widget=forms.TextInput(), required=False)
+    official_site_biography = forms.CharField(label='BIOGRAPHY', widget=forms.TextInput(), required=False)
+    wiki_discography = forms.CharField(label='WIKI_DISC', widget=forms.TextInput(), required=False)
+    wiki_biography = forms.CharField(label='WIKI_BIO', widget=forms.TextInput(), required=False)
     spotyfi = forms.CharField(label='SPOTYFI', widget=forms.TextInput(), required=False)
     youtube = forms.CharField(label='YOUTUBE', widget=forms.TextInput(), required=False)
-    recommend = forms.CharField(label='RECOMMEND_TRACK', widget=forms.TextInput(), required=False)
+    recommend = forms.CharField(label='RECOMMEND', widget=forms.TextInput(), required=False)
